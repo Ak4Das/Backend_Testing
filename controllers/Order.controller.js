a63ab4c1526271755174f3419f0ecce706c3c2a6
Akash Das <akashdas02052@gmail.com>
2026-05-23 18:23:20 +0530

    Add asyncHandler on order routes

--- controllers/Order.controller.js
+++ controllers/Order.controller.js
@@ -6,63 +6,16 @@ import {
   findOrderByIdAndUpdate,
   findOrderByIdAndDelete,
 } from "../services/Order.service.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
-export const fetchAllOrders = async (req, res) => {
-  try {
-    const allOrders = await getAllOrders()
-    res.status(200)
-    res.json(allOrders)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchAllOrders = asyncHandler(getAllOrders)
 
-export const fetchOrderByUserId = async (req, res) => {
-  try {
-    const allOrders = await getOrderByUserId(req.params.id)
-    res.status(200)
-    res.json(allOrders)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchOrderByUserId = asyncHandler(getOrderByUserId)
 
-export const fetchOrderByOrderId = async (req, res) => {
-  try {
-    const order = await getOrderByOrderId(req.params.id)
-    res.status(200)
-    res.json(order)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchOrderByOrderId = asyncHandler(getOrderByOrderId)
 
-export const postNewOrder = async (req, res) => {
-  try {
-    const savedOrder = await saveNewOrder(req.body)
-    res.status(200)
-    res.json(savedOrder)
-  } catch (error) {
-    throw error
-  }
-}
+export const postNewOrder = asyncHandler(saveNewOrder)
 
-export const fetchOrderByIdAndUpdate = async (req, res) => {
-  try {
-    const updatedOrder = await findOrderByIdAndUpdate(req.params.id, req.body)
-    res.status(200)
-    res.json(updatedOrder)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchOrderByIdAndUpdate = asyncHandler(findOrderByIdAndUpdate)
 
-export const fetchOrderByIdAndDelete = async (req, res) => {
-  try {
-    const deletedOrder = await findOrderByIdAndDelete(req.params.id)
-    res.status(200)
-    res.json(deletedOrder)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchOrderByIdAndDelete = asyncHandler(findOrderByIdAndDelete)
--- services/Order.service.js
+++ services/Order.service.js
@@ -1,18 +1,20 @@
 import OrderModel from "../models/Order.model.js"
 
-export const getAllOrders = async () => {
+export const getAllOrders = async (req, res) => {
   try {
     const allOrders = await OrderModel.find()
-    return allOrders
+    res.status(200)
+    res.json(allOrders)
   } catch (error) {
     throw error
   }
 }
 
-export const getOrderByUserId = async (id) => {
+export const getOrderByUserId = async (req, res) => {
   try {
-    const allOrders = await OrderModel.find({ userId: id })
-    return allOrders
+    const allOrders = await OrderModel.find({ userId: req.params.id })
+    res.status(200)
+    res.json(allOrders)
   } catch (error) {
     throw error
   }
@@ -20,38 +22,46 @@ export const getOrderByUserId = async (id) => {
 
 export const getOrderByOrderId = async (id) => {
   try {
-    const allOrders = await OrderModel.find({ id })
-    return allOrders
+    const order = await OrderModel.find({ id: req.params.id })
+    res.status(200)
+    res.json(order)
   } catch (error) {
     throw error
   }
 }
 
-export const saveNewOrder = async (newOrder) => {
+export const saveNewOrder = async (req, res) => {
   try {
-    const NewOrder = new OrderModel(newOrder)
+    const NewOrder = new OrderModel(req.body)
     await NewOrder.save()
-    return NewOrder
+    res.status(200)
+    res.json(NewOrder)
   } catch (error) {
     throw error
   }
 }
 
-export const findOrderByIdAndUpdate = async (id, dataToUpdate) => {
+export const findOrderByIdAndUpdate = async (req, res) => {
   try {
+    const id = req.params.id
+    const dataToUpdate = req.body
     const order = await OrderModel.findOneAndUpdate({ id: id }, dataToUpdate, {
       new: true,
     })
-    return order
+    res.status(200)
+    res.json(order)
   } catch (error) {
     throw error
   }
 }
 
-export const findOrderByIdAndDelete = async (id) => {
+export const findOrderByIdAndDelete = async (req, res) => {
   try {
-    const order = await OrderModel.findOneAndDelete({ id: id })
-    return order
+    const deletedOrder = await OrderModel.findOneAndDelete({
+      id: req.params.id,
+    })
+    res.status(200)
+    res.json(deletedOrder)
   } catch (error) {
     throw error
   }
