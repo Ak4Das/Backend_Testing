5344d1536f6b8836c675ee40178f20238ce01280
Akash Das <akashdas02052@gmail.com>
2026-05-24 01:24:43 +0530

    Implement error handling on category routes

--- services/Categories.service.js
+++ services/Categories.service.js
@@ -1,4 +1,5 @@
 import CategoryModel from "../models/Category.model.js"
+import { BadRequestError, NotFoundError } from "../utils/customErrorHandler.js"
 
 export const fetchAllCategories = async (req, res) => {
   try {
@@ -12,7 +13,13 @@ export const fetchAllCategories = async (req, res) => {
 
 export const fetchCategory = async (req, res) => {
   try {
+    if (!req.params.category) {
+      throw new BadRequestError("Not mentioned category in the API.")
+    }
     const Category = await CategoryModel.find({ for: req.params.category })
+    if (Category.length === 0) {
+      throw new NotFoundError("Category not found.")
+    }
     res.status(200)
     res.json(Category)
   } catch (error) {
