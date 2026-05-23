5c8e5f388fe9ceeeb9fa0626ba9d5df0efd66fe0
Akash Das <akashdas02052@gmail.com>
2026-05-23 17:23:43 +0530

    Add asyncHandler on cloth routes

--- controllers/Cloth.controller.js
+++ controllers/Cloth.controller.js
@@ -8,87 +8,24 @@ import {
   getNewArriveCloths,
   getDistinctCommonCategories,
 } from "../services/Cloth.service.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
-export const fetchAllCloths = async (req, res) => {
-  try {
-    const allCloths = await getAllCloths()
-    res.status(200)
-    res.json(allCloths)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchAllCloths = asyncHandler(getAllCloths)
 
-export const fetchClothsById = async (req, res) => {
-  try {
-    const id = req.params.id
-    const cloth = await getClothById(id)
-    res.status(200)
-    res.json(cloth)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchClothsById = asyncHandler(getClothById)
 
-export const fetchNewArriveCloths = async (req, res) => {
-  try {
-    const cloth = await getNewArriveCloths()
-    res.status(200)
-    res.json(cloth)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchNewArriveCloths = asyncHandler(getNewArriveCloths)
 
-export const fetchDistinctCommonCategories = async (req, res) => {
-  try {
-    const cloth = await getDistinctCommonCategories()
-    res.status(200)
-    res.json(cloth)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchDistinctCommonCategories = asyncHandler(
+  getDistinctCommonCategories,
+)
 
-export const fetchOfferOnACategory = async (req, res) => {
-  try {
-    const commonCategory = req.params.commonCategory
-    const cloth = await getOfferOnACategory(commonCategory)
-    res.status(200)
-    res.json(cloth)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchOfferOnACategory = asyncHandler(getOfferOnACategory)
 
-export const fetchClothsByMainCategory = async (req, res) => {
-  try {
-    const mainCategory = req.params.mainCategory
-    const cloths = await getClothsByMainCategory(mainCategory)
-    res.status(200)
-    res.json(cloths)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchClothsByMainCategory = asyncHandler(getClothsByMainCategory)
 
-export const fetchClothsByCommonCategory = async (req, res) => {
-  try {
-    const commonCategory = req.params.commonCategory
-    const cloths = await getClothsByCommonCategory(commonCategory)
-    res.status(200)
-    res.json(cloths)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchClothsByCommonCategory = asyncHandler(
+  getClothsByCommonCategory,
+)
 
-export const fetchByIdAndUpdate = async (req, res) => {
-  try {
-    const updatedCloth = await findByIdAndUpdate(req.params.id, req.body)
-    res.status(200)
-    res.json(updatedCloth)
-  } catch (error) {
-    throw error
-  }
-}
+export const fetchByIdAndUpdate = asyncHandler(findByIdAndUpdate)
--- services/Cloth.service.js
+++ services/Cloth.service.js
@@ -1,43 +1,49 @@
 import ClothModel from "../models/Cloth.model.js"
 
-export const getAllCloths = async () => {
+export const getAllCloths = async (req, res) => {
   try {
     const allCloths = await ClothModel.find()
-    return allCloths
+    res.status(200)
+    res.json(allCloths)
   } catch (error) {
     throw error
   }
 }
 
-export const getClothById = async (id) => {
+export const getClothById = async (req, res) => {
   try {
+    const id = req.params.id
     const cloth = await ClothModel.findOne({ id: id })
-    return cloth
+    res.status(200)
+    res.json(cloth)
   } catch (error) {
     throw error
   }
 }
 
-export const getNewArriveCloths = async () => {
+export const getNewArriveCloths = async (req, res) => {
   try {
     const cloth = await ClothModel.find({ newArrival: true })
-    return cloth
+    res.status(200)
+    res.json(cloth)
   } catch (error) {
     throw error
   }
 }
 
-export const getDistinctCommonCategories = async () => {
+export const getDistinctCommonCategories = async (req, res) => {
   try {
     const categories = await ClothModel.distinct("commonCategory")
-    return categories
+    res.status(200)
+    res.json(categories)
   } catch (error) {
     throw error
   }
 }
 
-export const getOfferOnACategory = async (commonCategory) => {
+export const getOfferOnACategory = async (req, res) => {
   try {
+    const commonCategory = req.params.commonCategory
     const cloth = await ClothModel.find({
       commonCategory,
       $expr: {
@@ -55,36 +61,44 @@ export const getOfferOnACategory = async (commonCategory) => {
         ],
       },
     })
-    return cloth
+    res.status(200)
+    res.json(cloth)
   } catch (error) {
     throw error
   }
 }
 
-export const getClothsByMainCategory = async (mainCategory) => {
+export const getClothsByMainCategory = async (req, res) => {
   try {
+    const mainCategory = req.params.mainCategory
     const cloths = await ClothModel.find({ mainCategory: mainCategory })
-    return cloths
+    res.status(200)
+    res.json(cloths)
   } catch (error) {
     throw error
   }
 }
 
-export const getClothsByCommonCategory = async (commonCategory) => {
+export const getClothsByCommonCategory = async (req, res) => {
   try {
+    const commonCategory = req.params.commonCategory
     const cloths = await ClothModel.find({ commonCategory })
-    return cloths
+    res.status(200)
+    res.json(cloths)
   } catch (error) {
     throw error
   }
 }
 
-export const findByIdAndUpdate = async (id, dataToUpdate) => {
+export const findByIdAndUpdate = async (req, res) => {
   try {
+    const id = req.params.id
+    const dataToUpdate = req.body
     const cloth = await ClothModel.findOneAndUpdate({ id: id }, dataToUpdate, {
       new: true,
     })
-    return cloth
+    res.status(200)
+    res.json(cloth)
   } catch (error) {
     throw error
   }
